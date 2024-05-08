8d31ef3c20a2505293e9d5a729527db463a6c06a
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-05-08 23:05:40 +0700

    Install react-router-dom scss bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import "./App.scss";
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">hello</div>;
+};
 
 export default App;
