d57db9cf609d7df43dccf6664c26ac7a26926f6d
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-05-10 15:54:54 +0700

    Design Video HomePage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,15 @@
+import videoHome from "../../assets/video/video-homepage.webm";
+
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className="home-container">
+      <div className="video-container">
+        <video autoPlay muted loop className="video">
+          <source src={videoHome} type="video/webm" />
+        </video>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
