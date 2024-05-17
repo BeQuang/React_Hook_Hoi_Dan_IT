0051b6ff85bade2d6d0f5846defc82c8b0e8f5dd
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-05-17 23:28:45 +0700

    Frontend Data Processing (Lodash GroupBy)

--- src/components/ListQuiz/DetailQuiz/DetailQuiz.js
+++ src/components/ListQuiz/DetailQuiz/DetailQuiz.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import _ from "lodash";
+
 import { getDataQuiz } from "../../../services/questionsService";
 
 function DetailQuiz() {
@@ -13,7 +15,31 @@ function DetailQuiz() {
 
   const fetchQuestions = async () => {
     const res = await getDataQuiz(quizId);
-    console.log("check >>>>>", res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionsDescription,
+            image = null;
+
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionsDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+
+          return { questionId: key, answers, questionsDescription, image };
+        })
+        .value();
+      console.log("check data >>>>", data);
+    }
   };
 
   return <div className="detail-quiz">DetailQuiz</div>;
